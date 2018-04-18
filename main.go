9dcea8206eccf5193f15f1265486e4ac1b2cff82
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-04-18 12:57:17 +0900

    雑にサービスをきった

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -12,17 +10,8 @@ import (
 	"github.com/dghubble/gologin/twitter"
 	"github.com/dghubble/oauth1"
 	twitterOAuth1 "github.com/dghubble/oauth1/twitter"
-	"github.com/dghubble/sessions"
+	"github.com/mitubaEX/DDD_practice/service"
 )
-
-const (
-	sessionName    = "example-twtter-app"
-	sessionSecret  = "example cookie signing secret"
-	sessionUserKey = "twitterID"
-)
-
-// sessionStore encodes and decodes session data stored in signed cookies
-var sessionStore = sessions.NewCookieStore([]byte(sessionSecret), nil)
 
 // Config configures the main ServeMux.
 type Config struct {
@@ -33,9 +22,9 @@ type Config struct {
 // New returns a new ServeMux with app routes.
 func New(config *Config) *http.ServeMux {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", welcomeHandler)
-	mux.Handle("/profile", requireLogin(http.HandlerFunc(profileHandler)))
-	mux.HandleFunc("/logout", logoutHandler)
+	mux.HandleFunc("/", service.Welcom)
+	mux.Handle("/profile", service.RequireLogin(http.HandlerFunc(service.ProfileHandler)))
+	mux.HandleFunc("/logout", service.Logout)
 	// 1. Register Twitter login and callback handlers
 	oauth1Config := &oauth1.Config{
 		ConsumerKey:    config.TwitterConsumerKey,
@@ -44,73 +33,8 @@ func New(config *Config) *http.ServeMux {
 		Endpoint:       twitterOAuth1.AuthorizeEndpoint,
 	}
 	mux.Handle("/twitter/login", twitter.LoginHandler(oauth1Config, nil))
-	mux.Handle("/twitter/callback", twitter.CallbackHandler(oauth1Config, issueSession(), nil))
+	mux.Handle("/twitter/callback", twitter.CallbackHandler(oauth1Config, service.Login(), nil))
 	return mux
-}
-
-// issueSession issues a cookie session after successful Twitter login
-func issueSession() http.Handler {
-	fn := func(w http.ResponseWriter, req *http.Request) {
-		ctx := req.Context()
-		twitterUser, err := twitter.UserFromContext(ctx)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		// 2. Implement a success handler to issue some form of session
-		session := sessionStore.New(sessionName)
-		session.Values[sessionUserKey] = twitterUser.ID
-		session.Save(w)
-		http.Redirect(w, req, "/profile", http.StatusFound)
-	}
-	return http.HandlerFunc(fn)
-}
-
-// welcomeHandler shows a welcome message and login button.
-func welcomeHandler(w http.ResponseWriter, req *http.Request) {
-	if req.URL.Path != "/" {
-		http.NotFound(w, req)
-		return
-	}
-	if isAuthenticated(req) {
-		http.Redirect(w, req, "/profile", http.StatusFound)
-		return
-	}
-	page, _ := ioutil.ReadFile("home.html")
-	fmt.Fprintf(w, string(page))
-}
-
-// profileHandler shows protected user content.
-func profileHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(w, `<p>You are logged in!</p><form action="/logout" method="post"><input type="submit" value="Logout"></form>`)
-}
-
-// logoutHandler destroys the session on POSTs and redirects to home.
-func logoutHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == "POST" {
-		sessionStore.Destroy(w, sessionName)
-	}
-	http.Redirect(w, req, "/", http.StatusFound)
-}
-
-// requireLogin redirects unauthenticated users to the login route.
-func requireLogin(next http.Handler) http.Handler {
-	fn := func(w http.ResponseWriter, req *http.Request) {
-		if !isAuthenticated(req) {
-			http.Redirect(w, req, "/", http.StatusFound)
-			return
-		}
-		next.ServeHTTP(w, req)
-	}
-	return http.HandlerFunc(fn)
-}
-
-// isAuthenticated returns true if the user has a signed session cookie.
-func isAuthenticated(req *http.Request) bool {
-	if _, err := sessionStore.Get(req, sessionName); err == nil {
-		return true
-	}
-	return false
 }
 
 // main creates and starts a Server listening.
---
+++ service/AuthService.go
@@ -0,0 +1,96 @@
+package service
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/dghubble/gologin/twitter"
+	"github.com/dghubble/sessions"
+)
+
+const (
+	sessionName    = "example-twtter-app"
+	sessionSecret  = "example cookie signing secret"
+	sessionUserKey = "twitterID"
+)
+
+// sessionStore encodes and decodes session data stored in signed cookies
+var sessionStore = sessions.NewCookieStore([]byte(sessionSecret), nil)
+
+func Welcom(w http.ResponseWriter, req *http.Request) {
+	welcomeHandler(w, req)
+}
+
+func Login() http.Handler {
+	return issueSession()
+}
+
+func Logout(w http.ResponseWriter, req *http.Request) {
+	logoutHandler(w, req)
+}
+
+// issueSession issues a cookie session after successful Twitter login
+func issueSession() http.Handler {
+	fn := func(w http.ResponseWriter, req *http.Request) {
+		ctx := req.Context()
+		twitterUser, err := twitter.UserFromContext(ctx)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		// 2. Implement a success handler to issue some form of session
+		session := sessionStore.New(sessionName)
+		session.Values[sessionUserKey] = twitterUser.ID
+		session.Save(w)
+		http.Redirect(w, req, "/profile", http.StatusFound)
+	}
+	return http.HandlerFunc(fn)
+}
+
+// welcomeHandler shows a welcome message and login button.
+func welcomeHandler(w http.ResponseWriter, req *http.Request) {
+	if req.URL.Path != "/" {
+		http.NotFound(w, req)
+		return
+	}
+	if isAuthenticated(req) {
+		http.Redirect(w, req, "/profile", http.StatusFound)
+		return
+	}
+	page, _ := ioutil.ReadFile("home.html")
+	fmt.Fprintf(w, string(page))
+}
+
+// profileHandler shows protected user content.
+func ProfileHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, `<p>You are logged in!</p><form action="/logout" method="post"><input type="submit" value="Logout"></form>`)
+}
+
+// logoutHandler destroys the session on POSTs and redirects to home.
+func logoutHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == "POST" {
+		sessionStore.Destroy(w, sessionName)
+	}
+	http.Redirect(w, req, "/", http.StatusFound)
+}
+
+// requireLogin redirects unauthenticated users to the login route.
+func RequireLogin(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, req *http.Request) {
+		if !isAuthenticated(req) {
+			http.Redirect(w, req, "/", http.StatusFound)
+			return
+		}
+		next.ServeHTTP(w, req)
+	}
+	return http.HandlerFunc(fn)
+}
+
+// isAuthenticated returns true if the user has a signed session cookie.
+func isAuthenticated(req *http.Request) bool {
+	if _, err := sessionStore.Get(req, sessionName); err == nil {
+		return true
+	}
+	return false
+}
